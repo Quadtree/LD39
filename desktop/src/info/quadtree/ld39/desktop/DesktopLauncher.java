cabcfd48c375684e2c0d021266e86c7254335776
Quadtree <quadtree@gmail.com>
2017-07-28 23:56:21 -0700

    Start

---
+++ desktop/src/info/quadtree/ld39/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package info.quadtree.ld39.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import info.quadtree.ld39.LD39;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD39(), config);
+	}
+}
