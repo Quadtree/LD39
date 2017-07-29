433d9f885c69b80057c7b72039f4e6f246c6b284
Quadtree <quadtree@gmail.com>
2017-07-29 00:06:32 -0700

    Updated

--- core/src/info/quadtree/ld39/LD39.java
+++ core/src/info/quadtree/ld39/LD39.java
@@ -5,29 +5,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class LD39 extends ApplicationAdapter {
+	public TextureAtlas atlas;
 	SpriteBatch batch;
+
 	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create() {
+
+		atlas = new TextureAtlas(Gdx.files.internal("main.atlas"));
+
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(atlas.createSprite("hab"), 0, 0);
+		batch.end();
 	}
 }
