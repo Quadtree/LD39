8739228939309a24a4b817cf558f79b483c30199
Quadtree <quadtree@gmail.com>
2017-07-30 17:51:39 -0700

    Desktop fix

--- desktop/src/info/quadtree/ld39/desktop/DesktopLauncher.java
+++ desktop/src/info/quadtree/ld39/desktop/DesktopLauncher.java
@@ -2,14 +2,13 @@ package info.quadtree.ld39.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 import info.quadtree.ld39.LD39;
 
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 
-		TexturePacker.processIfModified("../../raw_assets", ".", "./main");
+		// TexturePacker.processIfModified("../../raw_assets", ".", "./main");
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1024;
